11cd13f8868507d1810f746a5e255f71870ff038
User User <useruser1985official@gmail.com>
2026-06-17 18:25:30 -0300

    Atualizando Código

--- main.cpp
+++ main.cpp
@@ -8,22 +8,27 @@ int main() {
     setlocale(LC_ALL, "portuguese");
     system("title Algoritmos em C++ & color 0A");
 
-    int num, cont = 0;
+    int numeros[2][5];
 
-    cout << "Digite um número: ";
-    cin >> num;
+    // Linha 0
+    numeros[0][0] = 0;
+    numeros[0][1] = 2;
+    numeros[0][2] = 4;
+    numeros[0][3] = 6;
+    numeros[0][4] = 8;
 
-    while(true) { // Laço infinito
-        cont++;
+    // Linha 1
+    numeros[1][0] = 1;
+    numeros[1][1] = 3;
+    numeros[1][2] = 5;
+    numeros[1][3] = 7;
+    numeros[1][4] = 9;
 
-        if(cont > num) { // Entra na condição e interrompe o laço com break
-            break;
+    for(int l = 0; l < 2; l++) { // Linhas
+        for(int c = 0; c < 5; c++) { // Colunas
+            cout << numeros[l][c] << " "; // Não pula linha, só dá espaço
         }
-        else if(cont % 2 == 1) { // Para escrever só os pares
-            continue;
-        }
-
-        cout << cont << endl;
+        cout << endl; // Pula de linha, executado após o for interno terminar o laço dele
     }
 
     return 0;
